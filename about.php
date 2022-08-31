258e854aa02300758204b6b7b5f50b87a5a7c4e6
9gl <glunkad007@gmail.com>
2022-08-31 17:29:47 +0530

    First Commit

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include 'inc/header.php'; ?>
+    <h2>About</h2>
+    <p class="text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates sequi ullam quos voluptatibus et molestiae praesentium laborum dolores quisquam nam, placeat minima molestias! Quae quas itaque numquam, accusamus tempora repudiandae?
+    Quod dolorum odio ipsam iste doloremque provident perspiciatis dolor, dolorem fuga libero id exercitationem illum magni velit tempora nostrum voluptatum tenetur veniam, obcaecati eum. Voluptas sed fugit dolorem eos ipsam?
+    </p>
+    </div>
+<?php include 'inc/footer.php'; ?>
